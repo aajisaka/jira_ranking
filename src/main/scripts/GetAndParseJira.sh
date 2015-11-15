f8ddf82c400fbdaf7d75c3fe759dc87b4f5ed141
Akira Ajisaka <aajisaka@apache.org>
2015-11-16 05:29:36 +0900

    Incompatible change: GetAndParseJira script now accepts only 1 argument (the number of issues to get).

--- src/main/scripts/GetAndParseJira.sh
+++ src/main/scripts/GetAndParseJira.sh
@@ -15,32 +15,32 @@
 #    limitations under the License.
 #
 
-# Number of issues get per request. At most 100.
-SIZE=100
-
-if [ $# -lt 1 -o $# -gt 2 ]; then
-  echo "Usage: ./GetAndParseJira.sh <count> [<size>]" 1>&2
+if [ $# -ne 1 ]; then
+  echo "Usage: ./GetAndParseJira.sh <issues>" 1>&2
   exit 1
 fi
 
-COUNT=`expr $1 - 1`
+FILES=`expr $1 / 100 - 1`
+REMAINDER=`expr $1 % 100`
 
-if [ $# -eq 2 ]; then
-  if [ $SIZE -ge 1 -a $SIZE -le 100 ]; then
-    SIZE=$2
-  fi
+# Download json from ASF JIRA via REST API and parse it.
+# Number of issues per request is hard-coded to 100
+# because ASF JIRA accepts at most 100 issues per request.
+if [ $FILES -ge 0 ] ; then
+  PARALLEL=2
+  seq 0 $FILES | xargs -P$PARALLEL -n1 sh -c 'offset=`expr $0 \* 100`; wget -O $0.json "https://issues.apache.org/jira/rest/api/2/search?jql=project%20in%20(HADOOP%2C%20HDFS%2C%20MAPREDUCE%2C%20YARN)%20AND%20status%20in%20(Resolved%2C%20Closed)%20ORDER%20BY%20updated%20DESC&maxResults=100&startAt=$offset"'
+  seq 0 $FILES | xargs -P1 -n1 sh -c './jiraParse.py $0.json >> tmp.tsv'
 fi
 
-
-# Download JIRAs
-PARALLEL=2
-seq 0 $COUNT | xargs -P$PARALLEL -n1 sh -c 'offset=`expr $0 \* 100`; wget -O HADOOP$0.json "https://issues.apache.org/jira/rest/api/2/search?jql=project%20in%20(HADOOP%2C%20HDFS%2C%20MAPREDUCE%2C%20YARN)%20AND%20status%20in%20(Resolved%2C%20Closed)%20ORDER%20BY%20updated%20DESC&maxResults=100&startAt=$offset"'
-
-# Parse JSON
-seq 0 $COUNT | xargs -P1 -n1 sh -c './jiraParse.py HADOOP$0.json >> tmp.tsv'
+# Download and parse the remainder
+if [ $REMAINDER -gt 0 ] ; then
+  OFFSET=`expr $FILES \* 100 + 100`
+  wget -O reminder.json "https://issues.apache.org/jira/rest/api/2/search?jql=project%20in%20(HADOOP%2C%20HDFS%2C%20MAPREDUCE%2C%20YARN)%20AND%20status%20in%20(Resolved%2C%20Closed)%20ORDER%20BY%20updated%20DESC&maxResults=$REMAINDER&startAt=$OFFSET"
+  ./jiraParse.py reminder.json >> tmp.tsv
+fi
 
 # Remove JSON
-/bin/rm -f HADOOP*.json
+/bin/rm -f *.json
 
 # Get input format (issue_no, project, reporter, assignee) from parsed file
 sed 's/-/	/' tmp.tsv > tmp2.tsv
